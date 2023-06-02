d896c9104c686154645c14b7e11d25f427215b7f
Nicolay Kalinin <nikolajk202@gmail.com>
2023-06-02 15:42:20 +0300

    File pom.xml added

--- src/main/java/Basket.java
+++ src/main/java/Basket.java
@@ -1,4 +1,7 @@
+import com.google.gson.Gson;
+
 import java.io.*;
+import java.lang.reflect.Type;
 import java.util.Arrays;
 
 public class Basket {
@@ -10,50 +13,50 @@ public class Basket {
 
     }
 
-    public Basket(String[] goods, int[] prices){
+    public Basket(String[] goods, int[] prices) {
         this.goods = goods;
         this.prices = prices;
         this.quantities = new int[goods.length];
     }
 
 
-
     public void addToCard(int productNum, int amount) {
         quantities[productNum] += amount;
     }
+
     public void printCart() {
         int totalPrice = 0;
         System.out.println("Список покупок");
-        for (int i =0; i < goods.length; i++) {
+        for (int i = 0; i < goods.length; i++) {
             if (quantities[i] > 0) {
                 int currentPrice = prices[i] * quantities[i];
                 totalPrice += currentPrice;
-                System.out.println(goods[i] + " " + prices[i] + " руб/шт " + quantities[i] + " шт " + currentPrice+ " руб");
+                System.out.println(goods[i] + " " + prices[i] + " руб/шт " + quantities[i] + " шт " + currentPrice + " руб");
             }
         }
-    System.out.println("Итого: " + totalPrice + " руб");
+        System.out.println("Итого: " + totalPrice + " руб");
     }
 
     public void saveTxt(File textFile) throws FileNotFoundException {
         try (PrintWriter out = new PrintWriter(textFile)) {
-            for (String good: goods) {
+            for (String good : goods) {
                 out.print(good + " ");
             }
             out.println();
 
-            for (int price: prices) {
+            for (int price : prices) {
                 out.print(price + " ");
             }
             out.println();
 
-            for (int quantity: quantities) {
+            for (int quantity : quantities) {
                 out.print(quantity + " ");
             }
             out.println();
         }
     }
 
-    public static Basket loadFromTxtFile(File textFile) {
+    public Basket loadFromTxtFile(File textFile) {
         Basket basket = new Basket();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(textFile))) {
             String goodsStr = bufferedReader.readLine();
@@ -73,31 +76,32 @@ public class Basket {
             throw new RuntimeException(e);
         }
 
-        public static void saveJSON (File file){
-            try (PrintWriter writer = new PrintWriter(file)) {
-                Gson gson = new Gson();
-                String json = gson.toJson(this);
-                writer.print(json);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-
-            public static Basket loadFromJSONFile (File saveFile){
-            }
-            Basket basket = null;
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-                StringBuilder builder = new StringBuilder();
-                String Line = null;
-                while ((Line = reader.readLine()) != null) {
-                    builder.append(Line);
-                }
-                Gson gson = new Gson();
-                builder = gson.fromJson(builder.toString(), Basket.class);
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            return basket;
+        public static void saveJSON (File) {
+        try (PrintWriter writer = new PrintWriter(file)) {
+            Gson gson = new Gson();
+            String json = gson.toJson(this);
+            writer.print(json);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
+        }
+
+        public static void loadFromJSONFile(saveFile) {
+                basket = null;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            StringBuilder builder = new StringBuilder();
+            String Line = null;
+            while ((Line = reader.readLine()) != null) {
+                builder.append(Line);
+            }
+            Gson gson = new Gson();
+            builder = gson.fromJson(builder.toString(), (Type) Basket.class);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+            }
+
+        return basket;
     }
+}
